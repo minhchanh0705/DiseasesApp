9116dee163ca5880e50bfb5fd0a7f79a4af5888f
Minh Chanh <minhchanh0705@gmail.com>
2020-11-30 16:09:03 +0700

    new commit

--- src/container/DataDiseases.jsx
+++ src/container/DataDiseases.jsx
@@ -1,5 +1,6 @@
 import "../css/DataDiseases.css";
 import lstDiseases from "../json/lstDiseases.json";
+import dataPato from "../json/dataPato.json";
 import "roboto-fontface";
 import { ArrowUpward, ArrowDownward, Remove } from "@material-ui/icons";
 import { useTranslation } from "react-i18next";
@@ -10,9 +11,30 @@ function DataDiseases() {
   let lst = lstDiseases.disease;
   const { t, i18n } = useTranslation();
   const user = useContext(DiseasesContext);
+  var result = [];
+  var resultLastYear = [];
+  var resultThisYear = [];
+
+  for (var i in dataPato) {
+    result.push([i, dataPato[i]]);
+  }
+
+  let lastYear = parseInt(result[result.length - 1][0].slice(0, 4)) - 1;
+  let thisYear = parseInt(result[result.length - 1][0].slice(0, 4));
+  let thisWeek = result[result.length - 1][0].substr(-2);
+  let lastWeek = result[result.length - 2][0].substr(-2);
+  result.map((e) => {
+    e[0].slice(0, 4) === thisYear + ""
+      ? resultThisYear.push(e)
+      : resultLastYear.push(e);
+  });
+  let resultLastYearThisWeek = resultLastYear[thisWeek - 1];
+  let resultThisYearLastWeek = resultThisYear[lastWeek - 1];
+  let resultThisYearThisWeek = resultThisYear[thisWeek - 1];
+
   return (
     <div className="DataDiseases">
-      <p>{user.User.name}</p>
+      {/* <p>{user.User.name}</p> */}
       <button
         onClick={() => {
           i18n.language === "en"
@@ -38,29 +60,49 @@ function DataDiseases() {
           </tr>
 
           <tr>
-            <th className="headerTable casesHeader2">{t("Week")} 27</th>
-            <th className="headerTable casesHeader2">{t("Week")} 26</th>
+            <th className="headerTable casesHeader2">
+              {t("Week") + " " + thisWeek}
+            </th>
+            <th className="headerTable casesHeader2">
+              {t("Week") + " " + lastWeek}
+            </th>
             <th className="headerTable casesHeaderStatus">{t("Status")}</th>
-            <th className="headerTable casesHeader2">2017 {t("Week")} 27</th>
-            <th className="headerTable casesHeader2">2018 {t("Week")} 27</th>
-            <th className="headerTable deathsHeader2">{t("Week")} 27</th>
-            <th className="headerTable deathsHeader2">{t("Week")} 26</th>
+            <th className="headerTable casesHeader2">
+              {thisYear + " " + t("Week") + " " + thisWeek}
+            </th>
+            <th className="headerTable casesHeader2">
+              {lastYear + " " + t("Week") + " " + thisWeek}
+            </th>
+            <th className="headerTable deathsHeader2">
+              {t("Week") + " " + thisWeek}
+            </th>
+            <th className="headerTable deathsHeader2">
+              {t("Week") + " " + lastWeek}
+            </th>
             <th className="headerTable deathsHeaderStatus">{t("Status")}</th>
-            <th className="headerTable deathsHeader2">2017 {t("Week")} 27</th>
-            <th className="headerTable deathsHeader2">2018 {t("Week")} 27</th>
+            <th className="headerTable deathsHeader2">
+              {thisYear + " " + t("Week") + " " + thisWeek}
+            </th>
+            <th className="headerTable deathsHeader2">
+              {lastYear + " " + t("Week") + " " + thisWeek}
+            </th>
           </tr>
         </thead>
         <tbody>
           {lst.map((e, i) => {
-            let casesW27 = Math.floor(Math.random() * 2000);
-            let casesW26 = Math.floor(Math.random() * 2000);
-            let deathsW27 = Math.floor(Math.random() * 2000);
-            let deathsW26 = Math.floor(Math.random() * 2000);
+            let casesThisWeek = resultThisYearThisWeek[1][e.code].cases;
+            let casesLastWeek = resultThisYearLastWeek[1][e.code].cases;
+            let deathsThisWeek = resultThisYearThisWeek[1][e.code].deaths;
+            let deathsLastWeek = resultThisYearLastWeek[1][e.code].deaths;
 
-            let cases2017W27 = Math.floor(Math.random() * 2000) * 27;
-            let cases2018W27 = Math.floor(Math.random() * 2000) * 27;
-            let deaths2017W27 = Math.floor(Math.random() * 2000) * 27;
-            let deaths2018W27 = Math.floor(Math.random() * 2000) * 27;
+            let casesLastYearThisWeek =
+              resultLastYearThisWeek[1][e.code].cummulativeCases;
+            let casesThisYearThisWeek =
+              resultThisYearThisWeek[1][e.code].cummulativeCases;
+            let deathsLastYearThisWeek =
+              resultLastYearThisWeek[1][e.code].cummulativeDeaths;
+            let deathsThisYearThisWeek =
+              resultThisYearThisWeek[1][e.code].cummulativeDeaths;
             return (
               <tr key={i}>
                 <td className="contentTable diseaseName">
@@ -68,14 +110,13 @@ function DataDiseases() {
                 </td>
 
                 {/* Cases */}
-                <td className="contentTable casesContent">{casesW27}</td>
-                <td className="contentTable casesContent">{casesW26}</td>
-
-                {casesW27 > casesW26 ? (
+                <td className="contentTable casesContent">{casesThisWeek}</td>
+                <td className="contentTable casesContent">{casesLastWeek}</td>
+                {casesThisWeek > casesLastWeek ? (
                   <td className="contentTable upIcon">
                     <ArrowUpward></ArrowUpward>
                   </td>
-                ) : e.StatusCases === "decrease" ? (
+                ) : casesThisWeek < casesLastWeek ? (
                   <td className="contentTable downIcon">
                     <ArrowDownward></ArrowDownward>
                   </td>
@@ -84,14 +125,17 @@ function DataDiseases() {
                     <Remove></Remove>
                   </td>
                 )}
-
-                <td className="contentTable casesContent">{cases2017W27}</td>
-                <td className="contentTable casesContent">{cases2018W27}</td>
+                <td className="contentTable casesContent">
+                  {casesThisYearThisWeek}
+                </td>
+                <td className="contentTable casesContent">
+                  {casesLastYearThisWeek}
+                </td>
 
                 {/* Deaths */}
-                <td className="contentTable deathsContent">{deathsW27}</td>
-                <td className="contentTable deathsContent">{deathsW26}</td>
-                {deathsW27 > deathsW26 ? (
+                <td className="contentTable deathsContent">{deathsThisWeek}</td>
+                <td className="contentTable deathsContent">{deathsLastWeek}</td>
+                {deathsThisWeek > deathsLastWeek ? (
                   <td className="contentTable upIcon">
                     <ArrowUpward></ArrowUpward>
                   </td>
@@ -104,8 +148,12 @@ function DataDiseases() {
                     <Remove></Remove>
                   </td>
                 )}
-                <td className="contentTable deathsContent">{deaths2017W27}</td>
-                <td className="contentTable deathsContent">{deaths2018W27}</td>
+                <td className="contentTable deathsContent">
+                  {deathsThisYearThisWeek}
+                </td>
+                <td className="contentTable deathsContent">
+                  {deathsLastYearThisWeek}
+                </td>
               </tr>
             );
           })}
